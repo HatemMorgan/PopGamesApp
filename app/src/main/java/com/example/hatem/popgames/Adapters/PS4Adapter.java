f49360e43c2a8fe0589ebe369e87d3b0786a594a
HatemMorgan <hatemmorgan17@gmail.com>
2016-11-02 16:36:33 +0200

    creating grid list item xml

---
+++ app/src/main/java/com/example/hatem/popgames/Adapters/PS4Adapter.java
@@ -0,0 +1,7 @@
+package com.example.hatem.popgames.Adapters;
+
+/**
+ * Created by hatem on 11/2/16.
+ */
+public class PS4Adapter {
+}
---
+++ app/src/main/java/com/example/hatem/popgames/Adapters/XBoxAdapter.java
@@ -0,0 +1,10 @@
+package com.example.hatem.popgames.Adapters;
+
+/**
+ * Created by hatem on 11/2/16.
+ */
+public class XBoxAdapter {
+
+    
+
+}

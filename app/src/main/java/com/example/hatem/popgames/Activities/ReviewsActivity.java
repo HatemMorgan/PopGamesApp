3246caa91cc5dd63beb368cd86b415baa38f6f7f
HatemMorgan <hatemmorgan17@gmail.com>
2016-11-23 13:26:06 +0200

    xml files for the review layout created

---
+++ app/src/main/java/com/example/hatem/popgames/Activities/ReviewsActivity.java
@@ -0,0 +1,17 @@
+package com.example.hatem.popgames.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hatem.popgames.R;
+
+public class ReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+
+
+    }
+}

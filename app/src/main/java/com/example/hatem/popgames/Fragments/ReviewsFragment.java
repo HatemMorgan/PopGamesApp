365add241e290d043eeda1869b20b4e55163978d
HatemMorgan <hatemmorgan17@gmail.com>
2016-11-23 14:16:31 +0200

    getting data from bundle and populate reviews game details  with it

--- app/src/main/java/com/example/hatem/popgames/Fragments/ReviewsFragment.java
+++ app/src/main/java/com/example/hatem/popgames/Fragments/ReviewsFragment.java
@@ -12,15 +12,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.hatem.popgames.R;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReviewsFragment extends Fragment {
 
-    ImageView imageView_moviePoster;
-    TextView textView_movieTitle;
-    TextView textView_movieReleaseYear;
+    ImageView imageView_gameImage;
+    TextView textView_gameName;
+    TextView textView_gameReleaseYear;
     TextView textView_noReviews;
     ListView listView_review_list;
 
@@ -35,14 +36,23 @@ public class ReviewsFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_reviews, container, false);
 
-        imageView_moviePoster = (ImageView) rootView.findViewById(R.id.imageView_Review_moviePoster);
-        textView_movieTitle = (TextView) rootView.findViewById(R.id.textView_Review_movieTitle);
-        textView_movieReleaseYear = (TextView) rootView.findViewById(R.id.textView_review_movieYeat);
+        imageView_gameImage = (ImageView) rootView.findViewById(R.id.imageView_Review_gameImage);
+        textView_gameName = (TextView) rootView.findViewById(R.id.textView_Review_gameName);
+        textView_gameReleaseYear = (TextView) rootView.findViewById(R.id.textView_review_gameReleaseYear);
         listView_review_list = (ListView) rootView.findViewById(R.id.listView_Reviews_list);
         textView_noReviews = (TextView) rootView.findViewById(R.id.textView_NoReviews);
 
-        Bundle bundle = getActivity().getIntent().getExtras();
 
+        // getting passed data from bundle
+        Bundle bundle = getActivity().getIntent().getExtras();
+        String gameName = bundle.getString("gameName");
+        String gameReleaseYear = bundle.getString("gameReleaseYear");
+        String gameImageUrl = bundle.getString("gameImageUrl");
+
+        // populating the view
+        Picasso.with(getActivity()).load(gameImageUrl).placeholder(R.drawable.progress_animation).into(imageView_gameImage);
+        textView_gameName.setText(gameName);
+        textView_gameReleaseYear.setText(gameReleaseYear);
 
 
         return rootView;
@@ -54,5 +64,8 @@ public class ReviewsFragment extends Fragment {
 
 
     }
+
+
+    
 
 }
